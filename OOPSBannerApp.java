c660980d628addcabf7d27686dbe6894a7998323
nd7815-sys <nd7815@srmist.edu.in>
2026-03-02 08:39:32 +0530

    Refactor Banner Logic into Functions

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,53 +1,51 @@
-/**
- * OOPSBannerApp UC5 - Render OOPS as Banner using Inline Array Initialization
- * @author Developer
- * @version 5.0
- */
-
 public class OOPSBannerApp {
 
+    public static String[] getOPattern() {
+        return new String[]{
+            " ***** ",
+            "*     *",
+            "*     *",
+            "*     *",
+            " ***** "
+        };
+    }
+
+   
+    public static String[] getPPattern() {
+        return new String[]{
+            " ***** ",
+            "*     *",
+            " ***** ",
+            "*      ",
+            "*      "
+        };
+    }
+
+   
+    public static String[] getSPattern() {
+        return new String[]{
+            " ***** ",
+            "*      ",
+            " ***** ",
+            "      *",
+            " ***** "
+        };
+    }
+
+   
     public static void main(String[] args) {
 
-        String[] lines = {
+        String[] oPattern = getOPattern();
+        String[] pPattern = getPPattern();
+        String[] sPattern = getSPattern();
 
-            String.join("   ",
-                " ***** ",
-                " ***** ",
-                " ***** ",
-                " ***** "
-            ),
-
-            String.join("   ",
-                "*     *",
-                "*     *",
-                "*     *",
-                "*     *"
-            ),
-
-            String.join("   ",
-                "*     *",
-                "*     *",
-                "*****  ",
-                "*****  "
-            ),
-
-            String.join("   ",
-                "*     *",
-                "*     *",
-                "*      ",
-                "     * "
-            ),
-
-            String.join("   ",
-                " ***** ",
-                " ***** ",
-                "*      ",
-                "*****  "
-            )
-        };
-
-        for (String line : lines) {
-            System.out.println(line);
+        for (int i = 0; i < oPattern.length; i++) {
+            System.out.println(
+                oPattern[i] + "  " +
+                oPattern[i] + "  " +
+                pPattern[i] + "  " +
+                sPattern[i]
+            );
         }
     }
 }
